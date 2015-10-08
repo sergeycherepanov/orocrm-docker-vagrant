671cc509d3486336daca134506757a13698fdd85
SergeyCherepanov <sergey@cherepanov.org.ua>
2015-10-08 18:42:57 +0400

    Logs normalization

--- image-base/bin/wait-install.sh
+++ image-base/bin/wait-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-until [ ! -f /var/www/app/config/parameters.yml ] || [ 0 -lt `cat /var/www/app/config/parameters.yml | grep ".*installed:\s*[\']\{0,1\}[a-zA-Z0-9\:\+\-]\{1,\}[\']\{0,1\}" | wc -l` ]
+until [ ! -f /var/www/app/config/parameters.yml ] || [ 0 -lt `cat /var/www/app/config/parameters.yml | grep ".*installed:\s*[\']\{0,1\}[a-zA-Z0-9\:\+\-]\{1,\}[\']\{0,1\}" | grep -v "null" | wc -l` ]
 do
     sleep 2;
 done
